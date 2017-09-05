0ee9ecbb9b2b663f13043cf1bf2511788e1d0ddb
Nick Celaya <NickCelaya07@gmail.com>
2017-09-04 20:23:10 -0600

    initial commit

---
+++ public/js/controller.js
@@ -0,0 +1,24 @@
+angular.module("testApp").controller("mainCtrl", function($scope, mainService){
+
+$scope.numOne;
+$scope.numTwoo;
+
+$scope.sum
+
+
+$scope.add = (numOne, numTwo) => {
+  $scope.sum = parseInt(numOne) + parseInt(numTwo);
+  return $scope.sum
+}
+
+$scope.clear = () => {
+
+     $scope.numOne = "";
+     $scope.numTwo = "";
+     $scope.sum = "";
+
+};
+
+
+//end of controller
+});
---
+++ public/js/service.js
@@ -0,0 +1,6 @@
+angular.module("testApp").service("mainService", function($http){
+
+this.serviceTest = "Nick Service is up and running"
+
+
+});
